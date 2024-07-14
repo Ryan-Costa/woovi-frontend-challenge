0ff4a618d9e83cd13ffee5a4751c992bcb006a96
Ryan <ryansouzadev@gmail.com>
2024-07-14 16:45:16 -0300

    feat: changing amount input to use react-hook-form and zod

--- src/components/input-select-amount.tsx
+++ src/components/input-select-amount.tsx
@@ -5,20 +5,37 @@ import {
   IconButton,
   useTheme,
 } from "@mui/material";
-import { ChangeEvent, FormEvent, useContext, useState } from "react";
+import { ChangeEvent, useContext, useState } from "react";
 import { AmountContext } from "../context/amount-provider";
 import { useNavigate } from "react-router-dom";
 import NavigationOutlinedIcon from "@mui/icons-material/NavigationOutlined";
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const inputAmountSchema = z.object({
+  amount: z.string().min(1, "Digite um número").max(13),
+});
+
+type InputAmountPropsSchama = z.infer<typeof inputAmountSchema>;
 
 export function InputSelectAmount() {
   const theme = useTheme();
   const [inputValue, setInputValue] = useState("");
-  const [error, setError] = useState(false);
   const { setTotalAmount } = useContext(AmountContext);
   const navigate = useNavigate();
 
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<InputAmountPropsSchama>({
+    resolver: zodResolver(inputAmountSchema),
+  });
+
   function formatCurrency(value: string) {
     const numberString = value.replace(/\D/g, "");
+    if (numberString === "") return "";
     const number = parseFloat(numberString) / 100;
 
     return number.toLocaleString("pt-BR", {
@@ -31,14 +48,13 @@ export function InputSelectAmount() {
     const { value } = event.target;
     const formattedValue = formatCurrency(value);
     setInputValue(formattedValue);
-    setError(false);
   }
 
-  function handleClick(event: FormEvent) {
-    event.preventDefault();
-
-    if (!inputValue || parseFloat(inputValue.replace(/\D/g, "")) === 0) {
-      setError(true);
+  function handleClick(data: InputAmountPropsSchama) {
+    if (
+      !data.amount ||
+      parseFloat(String(data.amount).replace(/\D/g, "")) === 0
+    ) {
       return;
     }
 
@@ -58,7 +74,7 @@ export function InputSelectAmount() {
     >
       <FormControl
         component="form"
-        onSubmit={handleClick}
+        onSubmit={handleSubmit(handleClick)}
         sx={{
           display: "flex",
           flexDirection: "row",
@@ -66,13 +82,14 @@ export function InputSelectAmount() {
         }}
       >
         <TextField
-          id="amount-to-pay"
+          {...register("amount")}
           label="Digite o valor"
           variant="outlined"
           value={inputValue}
           onChange={handleChange}
-          error={error}
-          helperText={error ? "Por favor, insira um valor válido." : ""}
+          error={!!errors.amount}
+          helperText={errors.amount?.message}
+          inputProps={{ maxLength: 13 }}
         />
         <IconButton
           type="submit"
